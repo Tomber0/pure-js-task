7716ce12dd24ee508e6e0f921f858c6ee447cbf7
Aleksey.Zaitsau <aleseyzaicev@gmail.com>
2023-12-11 05:12:03 +0300

    added terms block

--- src/script.js
+++ src/script.js
@@ -3,3 +3,29 @@ let termsAccepted = false;
 let termsShowed = false;
 let resolveTerms = null;
 const apiUrl = `http://localhost:8080`;
+
+function acceptTerms() {
+    resolveTerms();
+}
+
+const acceptTermsOfUse = async (termsOfUse) => {
+    const termsContainer = document.getElementById('terms');
+    termsOfUse.map((term, index) => {
+        let container = document.createElement('div');
+        let header = document.createElement('h3');
+        header.innerHTML = term.title;
+        let text = document.createElement('p');
+        if (term.text) {
+            text.innerHTML = term.text;
+        }
+        else {
+            text.innerHTML = term.content;
+        }
+        container.appendChild(header);
+        container.appendChild(text);
+        termsContainer.append(container);
+    });
+    return new Promise((resolve, reject) => {
+        resolveTerms = resolve;
+    });
+};
