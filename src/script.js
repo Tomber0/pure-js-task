052e3064b620f0a412cef1e010691f00bf172525
Aleksey.Zaitsau <aleseyzaicev@gmail.com>
2023-12-11 05:13:29 +0300

    added render images method

--- src/script.js
+++ src/script.js
@@ -30,6 +30,25 @@ const acceptTermsOfUse = async (termsOfUse) => {
     });
 };
 
+const renderImageToCanvas = async (imageUrl) => {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.crossOrigin = "Anonymous";
+        img.src = imageUrl;
+        img.onload = () => {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+            resolve(canvas);
+        };
+        img.onerror = (error) => {
+            reject(error);
+        };
+    });
+};
+
 const saveCanvasAsImage = (canvas, imageName) => {
     const link = document.createElement('a');
     link.download = imageName;
